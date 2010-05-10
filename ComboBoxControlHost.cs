43498dbebc796ad676033bca6fcb748131ba3870
Kaupisch-IT <github@kaupisch.it>
2010-05-10 10:32:12 +0000

    ComboBox, die ein anderes Control in der DropDown-Anzeige hosten kann.

---
+++ ComboBoxControlHost.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KaupischITC.Shared
+{
+	/// <summary>
+	/// Stellt ein ComboBox-Steuerelement dar, das andere Steuerelemente als DropDown anbieten kann
+	/// </summary>
+	public class ComboBoxControlHost : ComboBox
+	{
+		private ToolStripControlHost toolStripControlHost;						// kann Controls hosten
+		private ToolStripDropDown toolStripDropDown = new ToolStripDropDown();	// DropDown, das den ToolStripControlHost enthalten kann
+
+		// WindowMessages
+		private const int WM_USER = 0x0400;
+		private const int WM_REFLECT = WM_USER + 0x1C00;
+		private const int WM_COMMAND = 0x0111;
+		private const int CBN_DROPDOWN = 7;
+		private const int WM_CLICK = 513;
+
+	
+
+		/// <summary>
+		/// Gibt das Steuerelement, dass beim DropDown angezeigt wird, zurück oder legt dieses fest
+		/// </summary>
+		public Control HostedControl
+		{
+			get { return (toolStripControlHost!=null) ? this.toolStripControlHost.Control : null; }
+			set
+			{
+				this.toolStripDropDown.Items.Clear();
+				if (value!=null)
+				{
+					this.toolStripControlHost = new ToolStripControlHost(value);
+					this.toolStripDropDown.Items.Add(this.toolStripControlHost);
+				}
+			}
+		}
+
+
+
+		/// <summary>
+		/// Zeigt den DropDown an
+		/// </summary>
+		private void showDropDown()
+		{
+			if (this.toolStripDropDown != null)
+			{
+				this.toolStripControlHost.Control.Width = this.DropDownWidth;
+				this.toolStripControlHost.Control.Height = this.DropDownHeight;
+				this.toolStripDropDown.Show(this,0,this.Height);
+			}
+		}
+
+
+		/// <summary>
+		/// Retrieves the high-order word from the given 32-bit value. 
+		/// </summary>
+		/// <param name="value">Specifies the value to be converted. </param>
+		/// <returns>The high-order word of the specified value. </returns>
+		private static int HIWORD(int value)
+		{
+			return (value >> 16) & 0xffff;
+		}
+
+
+		/// <summary>
+		/// Verarbeitet Windows-Meldungen
+		/// </summary>
+		/// <param name="message">Die zu verarbeitende Windows-Message</param>
+		protected override void WndProc(ref Message message)
+		{
+			if ((message.Msg==(WM_REFLECT + WM_COMMAND) && HIWORD((int)message.WParam)==CBN_DROPDOWN) || message.Msg == WM_CLICK)
+				this.showDropDown();
+			else
+				base.WndProc(ref message);
+		}
+
+
+		/// <summary>
+		/// Gibt nicht mehr verwendete Ressourcen frei
+		/// </summary>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+				if (this.toolStripDropDown!=null)
+					this.toolStripDropDown.Dispose();
+			base.Dispose(disposing);
+		}
+	}
+}
