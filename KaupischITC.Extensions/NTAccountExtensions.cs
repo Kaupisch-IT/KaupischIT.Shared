102a92c280dff992e9a292787fa740d5938ed531
Kaupisch-IT <github@kaupisch.it>
2011-06-16 11:35:47 +0000

    - NTAccountExtensions hinzugefügt

---
+++ KaupischITC.Extensions/NTAccountExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Security.Principal;
+
+namespace KaupischITC.Extensions
+{
+	public static class NTAccountExtensions
+	{
+		[DebuggerStepThrough]
+		public static SecurityIdentifier ToSecurityIdentifier(this NTAccount ntAccount)
+		{
+			try { return ((SecurityIdentifier)ntAccount.Translate(typeof(SecurityIdentifier))); }
+			catch { return null; }
+		}
+	}
+}
