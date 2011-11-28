a18ac6241684492145016984e2184127e270668b
Kaupisch-IT <github@kaupisch.it>
2011-11-28 10:04:14 +0000

    - String.FormatWith hinzugefügt - Enum.HasFlags hinzugefügt

---
+++ KaupischITC.Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KaupischITC.Extensions
+{
+	public static class EnumExtensions
+	{
+		public static bool HasFlag(this Enum variable,Enum flag)
+		{
+			if (variable==null)
+				return false;
+			if (flag==null)
+				throw new ArgumentNullException("value");
+
+			if (!Enum.IsDefined(variable.GetType(),flag))
+				throw new ArgumentException(string.Format("Enumeration type mismatch. The flag is of type '{0}', was expecting '{1}'.",flag.GetType(),variable.GetType()));
+
+			ulong num = Convert.ToUInt64(flag);
+			return ((Convert.ToUInt64(variable) & num) == num);
+		}
+	}
+}
--- KaupischITC.Extensions/StringExtensions.cs
+++ KaupischITC.Extensions/StringExtensions.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Web.UI;
+using System.Web;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace KaupischITC.Extensions
 {
@@ -35,6 +39,61 @@ namespace KaupischITC.Extensions
 		public static string AsNullIfEmpty(this string value)
 		{
 			return (String.IsNullOrEmpty(value)) ? null : value;
+		}
+
+		
+		/// <summary>
+		/// Ersetzt ein oder mehrere Formatelemente in einer angegebenen Zeichenfolge durch die Zeichenfolgendarstellung der Eigenschaften eines angegebenen Objekts.
+		/// </summary>
+		/// <param name="format">Eine kombinierte Formatzeichenfolge</param>
+		/// <param name="source">Das Objekt mit den zu formatierenden Eigenschaften.</param>
+		/// <returns>Eine Kopie von format, in der die Formatelemente durch die Zeichenfolgendarstellung der entsprechenden Eigenschaften aus source ersetzt wurden.</returns>
+		public static string FormatWith(this string format,object source)
+		{
+			return StringExtensions.FormatWith(format,null,source);
+		}
+
+
+		/// <summary>
+		/// Ersetzt ein oder mehrere Formatelemente in einer angegebenen Zeichenfolge durch die Zeichenfolgendarstellung der Eigenschaften eines angegebenen Objekts.
+		/// </summary>
+		/// <param name="format">Eine kombinierte Formatzeichenfolge</param>
+		/// <param name="provider">Ein Objekt, das kulturspezifische Formatierungsinformationen bereitstellt. </param>
+		/// <param name="source">Das Objekt mit den zu formatierenden Eigenschaften.</param>
+		/// <returns>Eine Kopie von format, in der die Formatelemente durch die Zeichenfolgendarstellung der entsprechenden Eigenschaften aus source ersetzt wurden.</returns>
+		/// <remarks>http://james.newtonking.com/archive/2008/03/29/formatwith-2-0-string-formatting-with-named-variables.aspx</remarks>
+		public static string FormatWith(this string format,IFormatProvider provider,object source)
+		{
+			if (format == null)
+				throw new ArgumentNullException("format");
+
+			List<object> values = new List<object>();
+
+			Regex regex = new Regex(@"(?<start>\{)+(?<property>[\w\.\[\]]+)(?<format>:[^}]+)?(?<end>\})+",RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+			string rewrittenFormat = regex.Replace(format,
+				delegate(Match match)
+				{
+					Group startGroup = match.Groups["start"];
+					Group propertyGroup = match.Groups["property"];  
+					Group formatGroup = match.Groups["format"];
+					Group endGroup = match.Groups["end"]; 
+
+					if (propertyGroup.Value=="0")
+						values.Add(source);
+					else
+						try { values.Add(DataBinder.Eval(source,propertyGroup.Value)); }
+						catch (HttpException e) { throw new FormatException(null,e); }
+
+					int openingsCount = startGroup.Captures.Count;
+					int closingsCount = endGroup.Captures.Count;
+
+					if (openingsCount>closingsCount || openingsCount%2==0)
+						return match.Value;
+					else
+						return new string('{',openingsCount) + (values.Count-1) + formatGroup.Value + new string('}',closingsCount);
+				});
+
+			return string.Format(provider,rewrittenFormat,values.ToArray());
 		}
 	}
 }
