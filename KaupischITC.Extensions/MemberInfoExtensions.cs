e4d0f7ec76501ed91f965cb2e1671f352d1200d1
Kaupisch-IT <github@kaupisch.it>
2011-12-08 16:55:25 +0000

    - Erweiterungsmethode für MemberInfo hinzugefügt (GetMemberType)

---
+++ KaupischITC.Extensions/MemberInfoExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace KaupischITC.Extensions
+{
+	public static class MemberInfoExtensions
+	{
+		[DebuggerStepThrough]
+		public static Type GetMemberType(this MemberInfo memberInfo)
+		{
+			if (memberInfo is PropertyInfo)
+				return ((PropertyInfo)memberInfo).PropertyType;
+			else if (memberInfo is MethodInfo)
+				return ((MethodInfo)memberInfo).ReturnType;
+			else if (memberInfo is FieldInfo)
+				return ((FieldInfo)memberInfo).FieldType;
+			else
+				throw new ArgumentException(memberInfo.ToString());
+		}
+	}
+}
