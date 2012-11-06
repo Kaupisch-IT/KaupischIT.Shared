98ec61ca872580ffd9243f3a4735bb37fddf6f77
Kaupisch-IT <github@kaupisch.it>
2012-11-06 12:32:39 +0000

    - Erweiterungsmethode AddParameterWithValue für die IDbCommand-Schnittstelle hinzugefügt

---
+++ KaupischITC.Extensions/DbCommandExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+
+namespace KaupischITC.Extensions
+{
+	/// <summary>
+	/// Stellt Erweiterungsmethoden für die IDbCommand-Schnittstelle bereit
+	/// </summary>
+	public static class DbCommandExtensions
+	{
+		/// <summary>
+		/// Fügt am Ende der IDbParameterCollection einen Wert hinzu.
+		/// </summary>
+		/// <param name="dbCommand">der IDbCommand, zu dessen IDbParameterCollection ein Wert hinzugefügt werden soll</param>
+		/// <param name="name">der Name des Parameters</param>
+		/// <param name="value">der hinzuzufügende Wert</param>
+		/// <returns>das hinzugefügte IDataParameter-Objekt</returns>
+		public static IDataParameter AddParameterWithValue(this IDbCommand dbCommand,string name,object value)
+		{
+			IDataParameter result = dbCommand.CreateParameter();
+			result.ParameterName = name;
+			result.Value = value;
+			dbCommand.Parameters.Add(result);
+			return result;
+		}
+	}
+}
