649f8f4471c6744c74e85bfc276d409bd1c89305
Kaupisch-IT <github@kaupisch.it>
2012-12-17 13:26:05 +0000

    - optionales Argument aus GetColumnValue entfernt, da Methode sonst nicht in Ausdrucksbaumstrukturen verwendet werden konnte

--- KaupischITC.Extensions/DataRecordExtensions.cs
+++ KaupischITC.Extensions/DataRecordExtensions.cs
@@ -15,10 +15,23 @@ namespace KaupischITC.Extensions
 		/// <typeparam name="T">der Rückgabetyp, in den der Wert der Spalte gecastet werden soll</typeparam>
 		/// <param name="dataRecord">die Datenzeile, die die Werte enthält</param>
 		/// <param name="columnName">der Name der Spalte, deren Wert ermittelt werden soll</param>
+		/// <returns>der Wert der Spalte</returns>
+		[DebuggerStepThrough]
+		public static T GetColumnValue<T>(this IDataRecord dataRecord,string columnName)
+		{
+			return DataRecordExtensions.GetColumnValue<T>(dataRecord,columnName,false);
+		}
+
+		/// <summary>
+		/// Ermittelt den Wert der angegebenen Spalte
+		/// </summary>
+		/// <typeparam name="T">der Rückgabetyp, in den der Wert der Spalte gecastet werden soll</typeparam>
+		/// <param name="dataRecord">die Datenzeile, die die Werte enthält</param>
+		/// <param name="columnName">der Name der Spalte, deren Wert ermittelt werden soll</param>
 		/// <param name="changeType">gibt an, ob versucht werden soll, den Wert der Spalte in den Rückgabetypen zu konvertieren</param>
 		/// <returns>der Wert der Spalte</returns>
 		[DebuggerStepThrough]
-		public static T GetColumnValue<T>(this IDataRecord dataRecord,string columnName,bool changeType = false)
+		public static T GetColumnValue<T>(this IDataRecord dataRecord,string columnName,bool changeType)
 		{
 			object value = dataRecord[columnName];
 			return (value is DBNull) 
