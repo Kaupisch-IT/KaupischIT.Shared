e7424ff935526bd7f40fded608457c689ca4bb8b
Kaupisch-IT <github@kaupisch.it>
2013-02-04 15:59:33 +0000

    - Erweiterungsmethoden für SendMessage

--- KaupischITC.Extensions/ControlExtensions.cs
+++ KaupischITC.Extensions/ControlExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace KaupischITC.Extensions
@@ -31,5 +33,41 @@ namespace KaupischITC.Extensions
 			rectangle.Inflate(padding,padding);
 			return rectangle.Contains(Control.MousePosition);
 		}
+
+
+		/// <summary>
+		/// Sendet dem Fenster die angegebene Meldung zu
+		/// </summary>
+		/// <param name="control">das Fenster, an das die Meldung geschickt werden soll</param>
+		/// <param name="message">die Meldung</param>
+		/// <returns>das Ergebnis des Meldungsverarbeitens; sein Wert hängt von der gesendeten Nachricht ab</returns>
+		public static IntPtr SendMessage(this Control control,Message message)
+		{
+			return ControlExtensions.SendMessage(control,message.Msg,message.WParam,message.LParam);
+		}
+
+		/// <summary>
+		/// Sendet dem Fenster die angegebene Meldung zu
+		/// </summary>
+		/// <param name="control">das Fenster, an das die Meldung geschickt werden soll</param>
+		/// <param name="msg">die ID-Nummer der Meldung</param>
+		/// <param name="wParam">das WParam-Feld der Meldung</param>
+		/// <param name="lParam">das LParam-Feld der Meldung</param>
+		/// <returns>das Ergebnis des Meldungsverarbeitens; sein Wert hängt von der gesendeten Nachricht ab</returns>
+		public static IntPtr SendMessage(this Control control,Int32 msg,IntPtr wParam,IntPtr lParam)
+		{
+			return ControlExtensions.SendMessage(control.Handle,msg,wParam,lParam);
+		}
+
+		/// <summary>
+		/// Sendet dem Fenster die angegebene Meldung zu
+		/// </summary>
+		/// <param name="hWnd">das Fensterhandle der Meldung</param>
+		/// <param name="Msg">die ID-Nummer der Meldung</param>
+		/// <param name="wParam">das WParam-Feld der Meldung</param>
+		/// <param name="lParam">das LParam-Feld der Meldung</param>
+		/// <returns>das Ergebnis des Meldungsverarbeitens; sein Wert hängt von der gesendeten Nachricht ab</returns>
+		[DllImport("user32.dll",EntryPoint="SendMessage",CharSet = CharSet.Auto)]
+		private static extern IntPtr SendMessage(IntPtr hWnd,Int32 Msg,IntPtr wParam,IntPtr lParam);
 	}
 }
