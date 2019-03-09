d6bf90c18fcd6d945bf517b65486e6d5aa25c3e5
Kaupisch-IT <github@kaupisch.it>
2019-03-09 19:56:57 +0000

    - Erweiterungsmethode hinzugefügt, die das Datum und Uhrzeit des Builds anhand eines AssemblyNames (aus den standardmäßigen Build- und Revisionsnummern) ermittelt

--- KaupischIT.Extensions/AssemblyExtensions.cs
+++ KaupischIT.Extensions/AssemblyExtensions.cs
@@ -15,10 +15,7 @@ namespace KaupischIT.Extensions
 		/// <returns>Datum und Uhrzeit, wann die angegebene Assembly vom Compiler erstellt wurde (Sommerzeit wird nicht berücksichtigt)</returns>
 		public static DateTime GetBuildDateTime(this Assembly assembly)
 		{
-			// die Build-Zeit kann aus den standardmäßigen Build- und Revisionsnummern in der Assembly-Version ermittelt werden
-			Version version = assembly.GetName().Version;
-			return new DateTime(2000,1,1).AddDays(version.Build) // "Build"-Wert entspricht der Anzahl der Tage seit dem 01.01.2000
-				.AddSeconds(version.Revision*2);                 // "Revision"-Wert entspricht der Hälfte der Anzahl der Sekunden seit Mitternacht
+			return assembly.GetName().GetBuildDateTime();
 		}
 	}
 }
---
+++ KaupischIT.Extensions/AssemblyNameExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace KaupischIT.Extensions
+{
+	/// <summary>
+	/// Stellt Erweiterungsmethoden für die AssemblyName-Klasse bereit
+	/// </summary>
+	public static class AssemblyNameExtensions
+	{
+		/// <summary>
+		/// Ermittelt die Build-Zeit, an der die angegebene Assembly erstellt wurde (Sommerzeit wird nicht berücksichtigt)
+		/// </summary>
+		/// <param name="assembly">die Assembly, deren Build-Zeit ermittelt werden soll</param>
+		/// <returns>Datum und Uhrzeit, wann die angegebene Assembly vom Compiler erstellt wurde (Sommerzeit wird nicht berücksichtigt)</returns>
+		public static DateTime GetBuildDateTime(this AssemblyName assemblyName)
+		{
+			// die Build-Zeit kann aus den standardmäßigen Build- und Revisionsnummern ermittelt werden
+			Version version = assemblyName.Version;
+			return new DateTime(2000,1,1).AddDays(assemblyName.Version.Build) // "Build"-Wert entspricht der Anzahl der Tage seit dem 01.01.2000
+				.AddSeconds(assemblyName.Version.Revision*2);                 // "Revision"-Wert entspricht der Hälfte der Anzahl der Sekunden seit Mitternacht
+		}
+	}
+}
