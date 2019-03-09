869de14a79e020bd1d9d1d7385dad1282ada1eeb
Kaupisch-IT <github@kaupisch.it>
2019-03-09 19:41:15 +0000

    - Erweiterungsmethode hinzugefügt, die die Datum und Uhrzeit des Builds einer Assembly (aus den standardmäßigen Build- und Revisionsnummern) ermittelt

---
+++ KaupischIT.Extensions/AssemblyExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace KaupischIT.Extensions
+{
+	/// <summary>
+	/// Stellt Erweiterungsmethoden für die Assembly-Klasse bereit
+	/// </summary>
+	public static class AssemblyExtensions
+	{
+		/// <summary>
+		/// Ermittelt die Build-Zeit, an der die angegebene Assembly erstellt wurde (Sommerzeit wird nicht berücksichtigt)
+		/// </summary>
+		/// <param name="assembly">die Assembly, deren Build-Zeit ermittelt werden soll</param>
+		/// <returns>Datum und Uhrzeit, wann die angegebene Assembly vom Compiler erstellt wurde (Sommerzeit wird nicht berücksichtigt)</returns>
+		public static DateTime GetBuildDateTime(this Assembly assembly)
+		{
+			// die Build-Zeit kann aus den standardmäßigen Build- und Revisionsnummern in der Assembly-Version ermittelt werden
+			Version version = assembly.GetName().Version;
+			return new DateTime(2000,1,1).AddDays(version.Build) // "Build"-Wert entspricht der Anzahl der Tage seit dem 01.01.2000
+				.AddSeconds(version.Revision*2);                 // "Revision"-Wert entspricht der Hälfte der Anzahl der Sekunden seit Mitternacht
+		}
+	}
+}
