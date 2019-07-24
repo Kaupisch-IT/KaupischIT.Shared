506e3063fd3746043bb2a87d645ca1ec2c6e3b0e
Kaupisch-IT <github@kaupisch.it>
2019-07-24 11:39:04 +0000

    - Komponente zum Ansprechen der Console aus als Windows-Anwendung erstellten Anwendungen

---
+++ KaupischIT.Shared/Controls/ConsoleForWindowsApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KaupischIT.Shared
+{
+	public class ConsoleForWindowsApplication : IDisposable
+	{
+		[DllImport("kernel32.dll")]
+		private static extern bool AllocConsole();
+
+		[DllImport("kernel32.dll")]
+		private static extern bool AttachConsole(int pid);
+
+		[DllImport("kernel32.dll",SetLastError = true)]
+		private static extern bool FreeConsole();
+
+		public ConsoleForWindowsApplication()
+		{
+			if (!ConsoleForWindowsApplication.AttachConsole(-1))
+				ConsoleForWindowsApplication.AllocConsole();
+		}
+
+		public void Dispose()
+		{
+			ConsoleForWindowsApplication.FreeConsole();
+			SendKeys.SendWait("{ENTER}");
+		}
+	}
+}
