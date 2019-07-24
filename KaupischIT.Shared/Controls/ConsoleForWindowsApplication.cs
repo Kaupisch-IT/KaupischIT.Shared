7e4039f37efef0113a60efcc946985579b2f9f5f
Kaupisch-IT <github@kaupisch.it>
2019-07-24 11:41:18 +0000

    (- Code kommentiert)

--- KaupischIT.Shared/Controls/ConsoleForWindowsApplication.cs
+++ KaupischIT.Shared/Controls/ConsoleForWindowsApplication.cs
@@ -4,14 +4,22 @@ using System.Windows.Forms;
 
 namespace KaupischIT.Shared
 {
+	/// <summary>
+	/// Stellt den Zugriff auf die Console für als Windows-Anwendung erstellte Anwendungen bereit
+	/// (sodass z.B. Console.WriteLine benutzt werden kann und auch was auf der Console ausgibt)
+	/// </summary>
 	public class ConsoleForWindowsApplication : IDisposable
 	{
+		/// <summary> Allocates a new console for the calling process </summary>
+		/// <returns></returns>
 		[DllImport("kernel32.dll")]
 		private static extern bool AllocConsole();
 
+		/// <summary> Attaches the calling process to the console of the specified process. </summary>
 		[DllImport("kernel32.dll")]
 		private static extern bool AttachConsole(int pid);
 
+		/// <summary> Detaches the calling process from its console. </summary>
 		[DllImport("kernel32.dll",SetLastError = true)]
 		private static extern bool FreeConsole();
 
